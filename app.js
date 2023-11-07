1e2a2ebfdc13a6ffa16bec0a1c336c09418c7bcd
Sharan HS <136172023+sharan-hs@users.noreply.github.com>
2023-11-07 16:12:21 +0530

    Added another dropdown

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function dropdown() {
     let element = document.getElementById("dropdown-content");
     element.classList.toggle("active");
 }
+
+function dropdown2() {
+    let element2 = document.getElementById("dropdown2-content");
+    element2.classList.toggle("active");
+}
